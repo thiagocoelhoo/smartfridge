4c189214bcccee45e7b662e89738acac117743c0
vynijales <vynijales@gmail.com>
2024-08-19 01:27:48 -0300

    feat: create first version of fridge stock

--- lib/pages/my_fridge_page.dart
+++ lib/pages/my_fridge_page.dart
@@ -14,6 +14,14 @@ class MyFridgePage extends StatefulWidget {
     Product("Carne", 2),
     Product("Café", 1),
     Product("Arroz", 3),
+    Product("Pão", 10),
+    Product("Carne", 2),
+    Product("Café", 1),
+    Product("Arroz", 3),
+    Product("Pão", 10),
+    Product("Carne", 2),
+    Product("Café", 1),
+    Product("Arroz", 3),
   ];
 
   MyFridgePage({super.key});
@@ -28,21 +36,65 @@ class _MyHomePageState extends State<MyFridgePage> {
     return Scaffold(
         appBar: _appBar(),
         body: Center(
-            child: Column(
-          children: <Widget>[
-            SearchBar(leading: Icon(Icons.search)),
-            Container(
-                height: 600,
-                child: ListView(
-                  children: widget.products.map((Product p) {
+          child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  SearchBar(
+                    leading: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Icon(Icons.filter_alt),
+                    ),
+                    trailing: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Icon(Icons.search),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 16.0),
+                  Expanded(
+                      child: ListView(
+                    children: widget.products.map((Product p) {
                       return Card(
-                        child: Text(p.name, style: TextStyle(fontSize: 20),),
+                        color: Colors.transparent,
+                        elevation: 0,
                         margin: EdgeInsets.all(8),
+                        child: Container(
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  color: Colors.white,
+                                  width: 1.0,
+                                ),
+                              ),
+                            ),
+                            padding: EdgeInsets.all(16.0),
+                            child: Row(
+                              children: [
+                                Icon(Icons.fastfood),
+                                SizedBox(width: 16),
+                                Text(
+                                  p.name,
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            )),
                       );
-                  }).toList(),
-                ))
-          ],
-        )));
+                    }).toList(),
+                  ))
+                ],
+              )),
+        ),
+        floatingActionButton: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: FloatingActionButton.extended(
+            onPressed: () {
+            },
+            icon: Icon(Icons.add),
+            label: Text("Add Item"),
+          ),
+        ));
   }
 
   AppBar _appBar() {
