710211b3607d1f0f43b45197d0f4c0ca18635630
vynijales <vynijales@gmail.com>
2024-08-20 15:21:32 -0300

    feat: create recipe class

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,10 +1,12 @@
+import 'package:smartfridge/utils/duration_time.dart';
 import './product.dart';
 
 class Recipe {
-  String name;
-  String urlImage;
-  List<Product> ingredients;
+  final String name;
+  final String urlImage;
+  final List<Product> ingredients;
+  final List<String> stepByStep;
+  final DurationTime duration;
 
-  Recipe(this.name, this.urlImage, this.ingredients);
+  Recipe(this.name, this.urlImage, this.ingredients, this.stepByStep, this.duration);
 }
-
