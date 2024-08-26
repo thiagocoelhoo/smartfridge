a9d3c7ec6541807acd5b5deea43c022985e5f8de
vynijales <vynijales@gmail.com>
2024-08-26 14:11:01 -0300

    feat: omit highlights when searching

--- lib/pages/recipes_page.dart
+++ lib/pages/recipes_page.dart
@@ -64,10 +64,10 @@ class _RecipesPage extends State<RecipesPage> {
                     left: 12, bottom: 12, right: 12, top: 4),
                 child: SearchBar(
                   controller: _searchController,
-                  leading: const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Icon(Icons.filter_alt),
-                  ),
+                  // leading: const Padding(
+                  //   padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  //   child: Icon(Icons.filter_alt),
+                  // ),
                   trailing: const [
                     Padding(
                       padding: EdgeInsets.symmetric(horizontal: 8.0),
@@ -81,16 +81,19 @@ class _RecipesPage extends State<RecipesPage> {
               ),
               Column(
                 children: [
-                  const Text("Destaques", style: TextStyle(fontSize: 22)),
-                  const SizedBox(height: 10),
-                  Carousel(
-                    images: [
-                      ...recipeRepository.recipes.map((Recipe r) {
-                        return Image.asset(r.urlImage);
-                      }),
-                    ],
-                    height: 400,
-                  ),
+                  if (_searchController.text.isEmpty &&
+                      recipeRepository.recipes.isNotEmpty) ...[
+                    const Text("Destaques", style: TextStyle(fontSize: 22)),
+                    const SizedBox(height: 10),
+                    Carousel(
+                      images: [
+                        ...recipeRepository.recipes.map((Recipe r) {
+                          return Image.asset(r.urlImage);
+                        }),
+                      ],
+                      height: 400,
+                    ),
+                  ],
                   const SizedBox(height: 22),
                   ...(_filteredRecipes.isEmpty &&
                           _searchController.text.isNotEmpty
