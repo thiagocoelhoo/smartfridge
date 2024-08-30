bab95ad43571f3db2c8bb3a0abf306395a25696d
vynijales <vynijales@gmail.com>
2024-08-29 21:30:40 -0300

    refactor: move controller logic to repository

--- lib/repository/fridge_repository.dart
+++ lib/repository/fridge_repository.dart
@@ -1,41 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:smartfridge/models/product.dart';
-import 'package:smartfridge/controllers/fridge_controller.dart';
+import 'package:smartfridge/utils/quantity.dart';
 
 class FridgeRepository extends ChangeNotifier {
-  final FridgeController _controller = FridgeController();
+  final List<Product> _products = [];
 
-  List<Product> get products => _controller.products;
+  FridgeRepository() {
+    _loadInitialProducts();
+  }
+
+  List<Product> get products => List.unmodifiable(_products);
 
   List<Product> getProductsByName(String name) {
-    return _controller.getProductsByName(name);
+    return _products
+        .where((element) =>
+            element.name.toLowerCase().contains(name.toLowerCase()))
+        .toList();
   }
 
   void addProduct(Product product) {
-    _controller.addProduct(product);
+    final index = _products.indexWhere(
+        (element) => element.name.toLowerCase() == product.name.toLowerCase());
+    if (index != -1) {
+      _products[index].amount.add(product.amount);
+    } else {
+      _products.add(product);
+    }
     notifyListeners();
   }
 
   void removeProduct(Product product) {
-    _controller.removeProduct(product);
+    if (_products.contains(product)) {
+      _products.remove(product);
+    }
     notifyListeners();
   }
 
   void updateProduct(Product product) {
-    _controller.updateProduct(product);
+    final index = _products.indexWhere((element) => element.id == product.id);
+    final nameExists = _products.any((element) =>
+        element.name.toLowerCase() == product.name.toLowerCase() &&
+        element.id != product.id);
+    if (index != -1 && !nameExists) {
+      _products[index] = product;
+    }
     notifyListeners();
   }
 
   bool containsEnough(Product product) {
-    return _controller.containsEnough(product);
+    final index =
+        _products.indexWhere((element) => element.name == product.name);
+    if (index == -1) {
+      return false;
+    }
+    return _products[index].amount.value >= product.amount.value;
   }
 
   int hasInTheFridge(List<Product> products) {
-    return _controller.hasInTheFridge(products);
+    int count = 0;
+    for (final product in products) {
+      if (containsEnough(product)) {
+        count++;
+      }
+    }
+    return count;
   }
 
   void clear() {
-    _controller.clear();
+    _products.clear();
     notifyListeners();
+  }
+
+  void _loadInitialProducts() {
+    _products.addAll([
+      Product("Pão", Quantity(10, QuantityUnit.unit)),
+      Product("Carne", Quantity(1, QuantityUnit.kilogram)),
+      Product("Café", Quantity(200, QuantityUnit.milliliter)),
+      Product("Arroz", Quantity(2, QuantityUnit.kilogram)),
+      Product("Macarrão", Quantity(500, QuantityUnit.gram)),
+      Product("Massa de lasanha", Quantity(500, QuantityUnit.gram)),
+      Product("Presunto", Quantity(500, QuantityUnit.gram)),
+      Product("Queijo ralado", Quantity(500, QuantityUnit.gram)),
+    ]);
   }
 }
--- lib/repository/shopping_repository.dart
+++ lib/repository/shopping_repository.dart
@@ -1,33 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:smartfridge/models/product.dart';
-import 'package:smartfridge/controllers/shopping_controller.dart';
+import 'package:smartfridge/utils/quantity.dart';
 
 class ShoppingRepository extends ChangeNotifier {
-  final ShoppingController _controller = ShoppingController();
+  final List<Product> _products = [];
 
-  List<Product> get products => _controller.products;
+  ShoppingRepository() {
+    _loadInitialProducts();
+  }
+
+  List<Product> get products => List.unmodifiable(_products);
 
   List<Product> getProductsByName(String name) {
-    return _controller.getProductsByName(name);
+    return _products
+        .where((element) =>
+            element.name.toLowerCase().contains(name.toLowerCase()))
+        .toList();
   }
 
   void addProduct(Product product) {
-    _controller.addProduct(product);
+    final index = _products.indexWhere(
+        (element) => element.name.toLowerCase() == product.name.toLowerCase());
+    if (index != -1) {
+      _products[index].amount.value += product.amount.value;
+    } else {
+      final clonedProduct = Product(
+          product.name, Quantity(product.amount.value, product.amount.unit));
+      _products.add(clonedProduct);
+    }
     notifyListeners();
   }
 
   void removeProduct(Product product) {
-    _controller.removeProduct(product);
+    if (_products.contains(product)) {
+      _products.remove(product);
+    }
     notifyListeners();
   }
 
   void updateProduct(Product product) {
-    _controller.updateProduct(product);
+    final index = _products.indexWhere((element) => element.id == product.id);
+    final nameExists = _products.any((element) =>
+        element.name.toLowerCase() == product.name.toLowerCase() &&
+        element.id != product.id);
+    if (index != -1 && !nameExists) {
+      _products[index] = product;
+    }
     notifyListeners();
   }
 
   void clear() {
-    _controller.clear();
+    _products.clear();
     notifyListeners();
+  }
+
+  void _loadInitialProducts() {
+    _products.addAll([
+      Product("Arroz", Quantity(1, QuantityUnit.kilogram)),
+      Product("Feijão", Quantity(3, QuantityUnit.kilogram)),
+      Product("Carne", Quantity(1.5, QuantityUnit.kilogram)),
+      Product("Macarrão", Quantity(2, QuantityUnit.kilogram)),
+      Product("Óleo", Quantity(1, QuantityUnit.liter)),
+      Product("Sal", Quantity(1, QuantityUnit.kilogram)),
+    ]);
   }
 }
